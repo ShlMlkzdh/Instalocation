61bd7fedeedf1c8baf4f03e31e5c6d95a5448c18
Soheil Malekzadeh <soheil.malekzadeh@yinzcam.com>
2018-02-18 15:36:38 -0500

    Add the LocationViewController class

---
+++ Instalocation/Controller/LocationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LocationViewController.swift
+//  Instalocation
+//
+//  Created by Soheil Malekzadeh on 2/18/18.
+//  Copyright © 2018 Soheil Malekzadeh. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class LocationViewController: UIViewController, CLLocationManagerDelegate {
+    
+    private let locationManager: CLLocationManager = CLLocationManager()
+    
+}
